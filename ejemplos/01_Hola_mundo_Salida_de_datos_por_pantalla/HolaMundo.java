13f521472021e1e348e1bfffea653bed7ca7277c
silvia <silvia2004pepa@gmail.com>
2023-05-18 10:57:17 +0200

    Cambio externo 1

--- ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/HolaMundo.java
+++ ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/HolaMundo.java
@@ -5,6 +5,6 @@
  */
 public class HolaMundo { // Clase principal
   public static void main(String[] args) {
-    System.out.println("¡Hola mundo!");
+    System.out.println("¡Hasta luego mundo!");
   }
 }
